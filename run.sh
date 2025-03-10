924b6391e9e16a8bebb79d510e7e1cb4e29ea194
Rohan O'Dwyer <166503624+rohanod@users.noreply.github.com>
2025-03-10 12:17:37 +0100

    remove virtual environment creation from run script

--- run.sh
+++ run.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-
-if [ ! -d "venv" ]; then
-  echo "Creating virtual environment for ComfyUI Launcher..."
-  echo
-  echo
-  python3 -m venv venv
-  echo
-  echo
-fi
-
 echo "Installing required packages..."
 echo
-echo
-. venv/bin/activate
 pip install -r requirements.txt
 
 echo
